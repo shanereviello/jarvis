7d88b55d567adfb46e2b2c38e8d0f25baab5c74b
Shane Reviello <shanereviello@gmail.com>
2026-07-12 22:33:34 -0400

    deleted old crap

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-JARVIS_MEMORY_ROOT = os.getenv("JARVIS_MEMORY_ROOT", "memory")
-JARVIS_VAULT_ROOT = os.getenv("JARVIS_VAULT_ROOT", "")
